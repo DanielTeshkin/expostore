82accbc13816123f362c4359a34c62f7914550a3
ArturYusupov <YusupovArturrr@gmail.com>
2022-02-20 21:41:47 +0300

    added Dagger Hilt commit

---
+++ app/src/main/java/com/expostore/di/AppModule.kt
@@ -0,0 +1,29 @@
+package com.expostore.di
+
+import com.expostore.api.ServerApi
+import com.expostore.utils.Constants.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideServerApi(retrofit: Retrofit): ServerApi =
+        retrofit.create(ServerApi::class.java)
+}
---
+++ app/src/main/java/com/expostore/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.expostore.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://expostore.ru"
+    }
+}
